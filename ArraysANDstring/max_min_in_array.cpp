89fa506f7139f8dce21da816cd9b2db20b94f437
tarunagarwal99 <tarunprahladka965@gmail.com>
2022-03-23 17:43:54 +0530

    .

---
+++ ArraysANDstring/max_min_in_array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A[]={1,2,3,4,5};
+    int minN0=INT_MAX;
+    for(int i=0;i<5;i++){
+        cout<<A[i]<<" ";
+    }
+    return 0;
+}
