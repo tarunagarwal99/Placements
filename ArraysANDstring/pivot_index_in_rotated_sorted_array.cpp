57f423bfeb6991b2fac42528843d353cab09e16d
tarunagarwal99 <tarunprahladka965@gmail.com>
2022-06-10 22:19:56 +0530

    .

---
+++ ArraysANDstring/pivot_index_in_rotated_sorted_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+//we have given rotated sorted array and we have to find its pivot index
+int main() {
+       vector<int> A={2,2,3,0,1,1};
+       int n=A.size();
+       int l=0,r=n-1;
+       int pivot=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(A[mid]>=A[0]){
+                l=mid+1;   //means we ahve to move to right part of the array,
+            }
+            else if(A[mid]<A[0]){
+                pivot=mid;
+                r=mid-1;
+            }
+        }
+        cout<<pivot;
+    return 0;
+}
